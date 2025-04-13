08d715baf62c7144f395cedff473fcb8d339d895
acf199 <63128624+aferguson9806@users.noreply.github.com>
2025-04-13 19:19:21 -0400

    fix 3

--- balloonsMain.js
+++ balloonsMain.js
@@ -1,3 +1,17 @@
+try {
+    localStorage.setItem('test', '1');
+    localStorage.removeItem('test');
+    console.log('localStorage is working');
+} catch (e) {
+    console.error('localStorage is not available', e);
+}
+
+
+// Save game manually
+document.getElementById("saveButton").addEventListener("click", saveGame);
+
+
+
 balloonButton = document.getElementById("mainControl");
 balloonButton.addEventListener("click", blowBalloon);
 
